52ade7f8b9d9d4d37f428e32d007e2204fcf1186
VuCongCuu <congcuu1102@gmail.com>
2021-07-15 21:44:13 +0700

    bt

---
+++ task8/Task8_SQL2.sql
@@ -0,0 +1,79 @@
+﻿CREATE DATABASE Task8
+USE Task8
+
+CREATE TABLE SanPham(
+STT INT IDENTITY(1,1),
+TenSP NVARCHAR(50),
+MaSP NVARCHAR(30) PRIMARY KEY NOT NULL,
+MoTa NVARCHAR(50),
+DonVi NVARCHAR(30),
+Gia MONEY,
+SoLuongHienCo INT,
+Note TEXT
+)
+
+INSERT INTO SanPham (TenSP,MaSP,MoTa,DonVi,Gia,SoLuongHienCo) VALUES (N'Máy Tính DELL7510','7510',N'Hàng Bán Chạy',N'Chiếc',1000,50)
+INSERT INTO SanPham (TenSP,MaSP,MoTa,DonVi,Gia,SoLuongHienCo) VALUES (N'Điện Thoại NOKIA6570','6570',N'Hàng Thanh Lý',N'Chiếc',500,10)
+INSERT INTO SanPham (TenSP,MaSP,MoTa,DonVi,Gia,SoLuongHienCo) VALUES (N'Máy In SAMSUNG 450','450',N'Hàng Mới Nhập',N'Chiếc',200,20)
+INSERT INTO SanPham (TenSP,MaSP,DonVi,Gia,SoLuongHienCo) VALUES(N'Chuột Máy Tinh','123',N'Chiếc',700,15)
+INSERT INTO SanPham (TenSP,MaSP,DonVi,Gia,SoLuongHienCo) VALUES (N'Bàn Phím Máy Tính','456',N'Chiếc',100,20)
+SELECT * FROM SanPham
+
+--Xóa  dong
+DELETE FROM SanPham WHERE STT=5
+
+CREATE TABLE NhaSX(
+TenNXS NVARCHAR(50),
+MaNSX NVARCHAR(50),
+MaSP NVARCHAR(30),CONSTRAINT SP FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP),
+DiaChi NVARCHAR(50),
+SDT BIGINT
+
+)
+INSERT INTO NhaSX VALUES ('Asus','AS123','123','USA',1234567890)
+INSERT INTO NhaSX VALUES ('Asus','AS123','456','USA',1234567890)
+INSERT INTO NhaSX VALUES ('NOKIA','N123','6570','JAPAN',4567891230)
+INSERT INTO NhaSX VALUES ('Dell','D123','7510','USA',7418589630)
+INSERT INTO NhaSX VALUES ('Samsung','S123','450','KOREA',8526987890)
+
+
+SELECT * FROM NhaSX
+
+--Hiển thi tát cả nha sx
+SELECT TenNXS FROM NhaSX GROUP BY TenNXS
+
+--Hiển thi tất cả SP
+
+SELECT TenSP FROM SanPham
+
+--Liệt kê danh sach hang theo thư tuwh ngược ALPHABET
+
+SELECT TenNXS FROM NhaSX GROUP BY TenNXS ORDER BY TenNXS DESC 
+
+--liet ke san pham theo gia giam dan
+
+SELECT TenSP,Gia FROM SanPham ORDER BY Gia DESC
+
+--Hiển thị thông tin hãng ASUS
+
+SELECT * FROM NhaSX AS A
+INNER JOIN SanPham AS B ON B.MaSP=A.MaSP
+WHERE TenNXS='Asus'
+GO
+
+-- Liet ke san pham con it hon 20 chiec
+
+SELECT TenSP,SoLuongHienCo FROM SanPham WHERE SoLuongHienCo<20
+
+--liet ke sp của asus
+
+SELECT TenSP FROM SanPham AS A
+INNER JOIN NhaSX AS B ON B.MaSP=A.MaSP
+WHERE TenNXS='Asus'
+GO
+
+--So luong sp Cua hang bán
+ SELECT COUNT(TenSP) AS SoSP FROM SanPham
+--Số hãng mà cửa hàng có
+SELECT  COUNT(TenNXS) AS SoHang FROM NhaSX 
+
---
+++ task8/Task8_SQL6.sql
@@ -0,0 +1,77 @@
+﻿ CREATE DATABASE Task8_SQL6
+USE Task8_SQL6
+
+CREATE TABLE NhaXuatBan(
+TenNXB NVARCHAR(50),
+MaNXB NVARCHAR (10) PRIMARY KEY NOT NULL
+)
+
+INSERT INTO NhaXuatBan VALUES (N'Giáo Dục','GD')
+INSERT INTO NhaXuatBan VALUES (N'Kim Đồng','KD')
+SELECT * FROM NhaXuatBan
+
+CREATE TABLE Sach(
+TenSach NVARCHAR(50),
+MaSach NVARCHAR(10) PRIMARY KEY NOT NULL,
+TenTG NVARCHAR(50),
+MaNXB NVARCHAR(10),CONSTRAINT MA FOREIGN KEY (MaNXB) REFERENCES NhaXuatBan(MaNXB),
+LoaiSach NVARCHAR(40),
+LanXuatBan INT DEFAULT '1'
+)
+INSERT INTO Sach(TenSach,MaSach,TenTG,MaNXB,LoaiSach) VALUES (N'Toán Lớp 1','TOAN1',N'Vũ Ngọc Hân','GD',N'Toán Học')
+INSERT INTO Sach(TenSach,MaSach,TenTG,MaNXB,LoaiSach) VALUES (N'Văn Lớp 1','VAN1',N'Phạm Anh Khoa','GD',N'Văn Học')
+INSERT INTO Sach(TenSach,MaSach,TenTG,MaNXB,LoaiSach) VALUES (N'Tin Học Lớp 5','TIN5',N'Vũ Khắc Khang','KD',N'Tin Học')
+INSERT INTO Sach(TenSach,MaSach,TenTG,MaNXB,LoaiSach) VALUES (N'Khoa Học Lớp 5','KHOA5',N'Cao Thị Lan','KD',N'Khoa Học')
+
+ALTER TABLE Sach ADD NamXuatBan DATETIME DEFAULT GETDATE()
+DELETE TABLE S
+
+SELECT * FROM Sach
+
+CREATE TABLE Kho(
+MaSach NVARCHAR(10), CONSTRAINT SA FOREIGN KEY (MaSach) REFERENCES Sach(MaSach),
+Gia MONEY,
+SoLuong INT,
+Note TEXT
+)
+INSERT INTO Kho VALUES ('TOAN1','1000',100,N'Sách Ế')
+INSERT INTO Kho(MaSach,Gia,SoLuong) VALUES ('VAN1','2000',50)
+INSERT INTO Kho VALUES ('TIN5','6000',10,N'Sách Bán Chạy')
+INSERT INTO Kho(MaSach,Gia,SoLuong) VALUES ('KHOA5','4000',20)
+
+SELECT * FROM Kho
+
+--Liệt kê 10 cuốn sách có giá bán cao nhất
+
+--Tìm những cuốn sách có tiêu đề chứa từ “tin học”
+
+--Liệt kê các cuốn sách có tên bắt đầu với chữ “T” theo thứ tự giá giảm dần
+
+--Liệt kê các cuốn sách của nhà xuất bản Tri thức
+
+--Lấy thông tin chi tiết về nhà xuất bản xuất bản cuốn sách “Trí tuệ Do Thái”
+
+--Hiển thị các thông tin sau về các cuốn sách: Mã sách, Tên sách, Năm xuất bản, Nhà xuất bản,
+
+--Tìm cuốn sách có giá bán đắt nhất
+
+--Tìm cuốn sách có số lượng lớn nhất trong kho
+
+--Tìm các cuốn sách của tác giả “Eran Katz”
+
+--Giảm giá bán 10% các cuốn sách xuất bản từ năm 2008 trở về trước
+
+--Thống kê số đầu sách của mỗi nhà xuất bản
+
+
+--Thống kê số đầu sách của mỗi loại sách
+
+--
+
+--
+
+--
+
+--
+
+--
